7c2df83b35782a44f1b7d7bba539af7d5e8dca12
suupia <morita777k@icloud.com>
2024-03-26 19:54:44 +0900

    Add SpawnPlayer()

--- Assets/Scripts/Runtime/Player/PlayerSpawnerMonoSystem.cs
+++ Assets/Scripts/Runtime/Player/PlayerSpawnerMonoSystem.cs
@@ -9,11 +9,14 @@ namespace DungeonCrawler.Runtime.Player
     public class PlayerSpawnerMonoSystem : MonoBehaviour, IPlayerSpawnerMonoSystem
     {
         [SerializeField] PlayerController playerPrefab = null!;
-
+        const float PlayerSpawnHeight = 1.0f;    
+        
         public void SpawnPlayer(int x, int y)
         {
             Debug.Log($"Player spawn position: {x}, {y}");
-            var instancePos = new Vector2(3, 3); // convert grid pos to world pos
+            var spawnGridPosition = GridConverter.GridPositionToWorldPosition(new Vector2Int(x, y));
+            var spawnPosition = new Vector3(spawnGridPosition.x, PlayerSpawnHeight, spawnGridPosition.z);
+            Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
         }
     }
 }
