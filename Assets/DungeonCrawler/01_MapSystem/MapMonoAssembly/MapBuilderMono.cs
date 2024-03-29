0183f3c7f678a264b6405eae4462506e5ec17b5c
suupia <morita777k@icloud.com>
2024-03-29 16:45:07 +0900

    Subscribe _dungeonSwitcher.Floor to UpdateSprites()

--- Assets/DungeonCrawler/01_MapSystem/MapMonoAssembly/MapBuilderMono.cs
+++ Assets/DungeonCrawler/01_MapSystem/MapMonoAssembly/MapBuilderMono.cs
@@ -35,28 +35,25 @@ namespace DungeonCrawler.MapMonoAssembly
             _coordinate = coordinate;
             _dungeonSwitcher = dungeonSwitcher;
             
-            for(int i = 0; i<_coordinate.Length ; i++)
-            {
-                var vector = _coordinate.ToVector(i);
-                var tile = Instantiate(tilePrefab, GridConverter.GridPositionToWorldPosition(vector), Quaternion.identity);
-                tile.transform.localScale = new Vector3(GridConverter.GridSize, GridConverter.GridSize, GridConverter.GridSize);
-                _pool.Add(tile);
-            }
+            SetUp();
         }
 
         void SetUp()
         {
+            InstantiateTiles();
+            
+            SwitchNextDungeon(); // Create first dungeon
+            
             Observable.EveryValueChanged(this, _ => _dungeonSwitcher.Floor)
                 .Subscribe(_ =>
                 {
-                    SwitchNextDungeon();
+                    UpdateSprites(_dungeonSwitcher.CurrentDungeon);
                 }); 
         }
 
         void SwitchNextDungeon()
         {
             Debug.Log("SwitchNextDungeon");
-            ResetAllTiles();
             var nextMap = _dungeonSwitcher.SwitchNextDungeon();
             UpdateSprites(nextMap);
         }
@@ -67,9 +64,22 @@ namespace DungeonCrawler.MapMonoAssembly
                 tile.ResetSprites();
             }
         }
+        
+        void InstantiateTiles()
+        {
+            for(int i = 0; i<_coordinate.Length ; i++)
+            {
+                var vector = _coordinate.ToVector(i);
+                var tile = Instantiate(tilePrefab, GridConverter.GridPositionToWorldPosition(vector), Quaternion.identity);
+                tile.transform.localScale = new Vector3(GridConverter.GridSize, GridConverter.GridSize, GridConverter.GridSize);
+                _pool.Add(tile);
+            }
+        }
 
         void UpdateSprites(DungeonGridMap plainDungeon)
         {
+            ResetAllTiles();
+
             Debug.Log($"Length:{_coordinate.Length}");
             for(int i = 0; i<_coordinate.Length ; i++)
             {
