3f3eb45b39f725eb79c2ad56aaf14ffb379ad8e1
suupia <morita777k@icloud.com>
2024-03-29 21:50:48 +0900

    Implement staring battle

--- Assets/DungeonCrawler/00_DungeonCrawlerSystem/GameSystemInitializer.cs
+++ Assets/DungeonCrawler/00_DungeonCrawlerSystem/GameSystemInitializer.cs
@@ -13,21 +13,25 @@ namespace DungeonCrawler
     {
         DungeonBuilder _dungeonBuilder = null!;
         DungeonSwitcher _dungeonSwitcher = null!;
+        GameStateSwitcher _gameStateSwitcher = null!;
         
         [Inject]
         public void Construct(
             DungeonBuilder dungeonBuilder,
-            DungeonSwitcher dungeonSwitcher
+            DungeonSwitcher dungeonSwitcher,
+            GameStateSwitcher gameStateSwitcher
             )
         {
             _dungeonBuilder = dungeonBuilder;
             _dungeonSwitcher = dungeonSwitcher;
+            _gameStateSwitcher = gameStateSwitcher;
 
             SetUp();
         }
         void SetUp()
         {
             Debug.Log("GameSystemInitializer.SetUp()");
+            _gameStateSwitcher.EnterExploring();
 
         }
     }
