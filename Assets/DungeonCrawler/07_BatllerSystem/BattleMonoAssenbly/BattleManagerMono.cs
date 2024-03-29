a35bbace745546958c181291cb468eacaad7f6c0
no-la <nzyc0216@icloud.com>
2024-03-29 14:01:44 +0900

    add BattleManagerMono.cs

---
+++ Assets/DungeonCrawler/07_BatllerSystem/BattleMonoAssenbly/BattleManagerMono.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonCrawler.BattleSystem.BattleMonoAssenbly
+{
+    public class BattleManagerMono : MonoBehaviour
+    {
+    }
+}
