24bb7bc44bf1ba90faa6558bd8d262c0734f1864
no-la <nzyc0216@icloud.com>
2024-03-29 14:07:01 +0900

    add UpdateTurn() to BattleManager.cs

--- Assets/DungeonCrawler/07_BatllerSystem/BattleAssenbly/BattaleManager.cs
+++ Assets/DungeonCrawler/07_BatllerSystem/BattleAssenbly/BattaleManager.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using DungeonCrawler.EnemyAssenbly.Classes;
+using DungeonCrawler.PlayerAssembly.Classes;
 using UnityEngine;
 
 namespace DungeonCrawler.BattleSystem.BattleAssenbly
 {
     public class BattaleManager
     {
+        PlayerDomain _player;
+        EnemyDomain _enemy;
+
+        public BattaleManager(PlayerDomain player, EnemyDomain enemy)
+        {
+            _player = player;
+            _enemy = enemy;
+        }
+        
+        public void UpdateTurn()
+        {
+            // player attack
+            
+            _enemy.Attack(_player);
+        }
     }
 }
