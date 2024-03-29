e8c1ddfaf2aa3d858ba89edeb56634166095a757
no-la <nzyc0216@icloud.com>
2024-03-29 09:41:43 +0900

    fix reversal of spawner and controller

--- Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchControllerMono.cs
+++ Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchControllerMono.cs
@@ -1,5 +1,3 @@
-#nullable enable
-using DungeonCrawler.MapAssembly.Classes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -9,15 +7,6 @@ namespace DungeonCrawler
 {
     public class TorchControllerMono : MonoBehaviour
     {
-        [SerializeField] TorchControllerMono torchPrefab;
-        const float TorchSpawnHeight = 1.0f;
         
-        public void SpawnTorch(int x, int y)
-        {
-            Debug.Log($"Torch spawn position: {x}, {y}");
-            var spawnGridPosition = GridConverter.GridPositionToWorldPosition(new Vector2Int(x, y));
-            var spawnPosition = new Vector3(spawnGridPosition.x, TorchSpawnHeight, spawnGridPosition.z);
-            Instantiate(torchPrefab, spawnPosition, Quaternion.identity);
-        }
     }
 }
--- Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchSpawnerMono.cs
+++ Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchSpawnerMono.cs
@@ -1,18 +1,24 @@
+#nullable enable
+using DungeonCrawler.MapAssembly.Classes;
 using System.Collections;
 using System.Collections.Generic;
+using PlasticGui;
 using UnityEngine;
 
-public class TorchSpawnerMono : MonoBehaviour
+namespace  DungeonCrawler
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TorchSpawnerMono : MonoBehaviour
     {
-        
-    }
+        [SerializeField] TorchControllerMono torchPrefab;
+        const float TorchSpawnHeight = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void SpawnTorch(int x, int y)
+        {
+            Debug.Log($"Torch spawn position: {x}, {y}");
+            var spawnGridPosition = GridConverter.GridPositionToWorldPosition(new Vector2Int(x, y));
+            var spawnPosition = new Vector3(spawnGridPosition.x, TorchSpawnHeight, spawnGridPosition.z);
+            Instantiate(torchPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
+
