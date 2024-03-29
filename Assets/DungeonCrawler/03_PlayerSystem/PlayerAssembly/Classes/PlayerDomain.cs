15ad70cd2ed542f300203ff671b1b6088f273be7
no-la <nzyc0216@icloud.com>
2024-03-29 13:54:34 +0900

    add hp, attack, Attack() to both

--- Assets/DungeonCrawler/03_PlayerSystem/PlayerAssembly/Classes/PlayerDomain.cs
+++ Assets/DungeonCrawler/03_PlayerSystem/PlayerAssembly/Classes/PlayerDomain.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using DungeonCrawler.EnemyAssenbly.Classes;
 using UnityEngine;
 
 namespace DungeonCrawler.PlayerAssembly.Classes
 {
     public class PlayerDomain
     {
-        
+        public int hp;
+        int attack;
+
+        public void Attack(EnemyDomain target)
+        {
+        }
+
+        public void SacredAttack(EnemyDomain target)
+        {
+            
+        }
     }
 }
--- Assets/DungeonCrawler/04_EnemySystem/EnemyAssenbly/Classes/EnemyDomain.cs
+++ Assets/DungeonCrawler/04_EnemySystem/EnemyAssenbly/Classes/EnemyDomain.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using DungeonCrawler.PlayerAssembly.Classes;
 using UnityEngine;
 
 namespace DungeonCrawler.EnemyAssenbly.Classes
 {
     public class EnemyDomain
     {
+        public int hp;
+        int attack;
+
+        public void Attack(PlayerDomain target)
+        {
+            
+        }
     }
 }
