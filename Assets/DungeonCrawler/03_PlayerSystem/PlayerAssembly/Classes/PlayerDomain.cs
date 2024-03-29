a8e1b9296d72de6555ab540003857eb592e3030b
no-la <nzyc0216@icloud.com>
2024-03-29 13:45:15 +0900

    add namespace to PlayerDomain.cs and EnemyDomain.cs

--- Assets/DungeonCrawler/03_PlayerSystem/PlayerAssembly/Classes/PlayerDomain.cs
+++ Assets/DungeonCrawler/03_PlayerSystem/PlayerAssembly/Classes/PlayerDomain.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDomain : MonoBehaviour
+namespace DungeonCrawler.PlayerAssembly.Classes
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerDomain : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/DungeonCrawler/04_EnemySystem/EnemyAssenbly/Classes/EnemyDomain.cs
+++ Assets/DungeonCrawler/04_EnemySystem/EnemyAssenbly/Classes/EnemyDomain.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDomain : MonoBehaviour
+namespace DungeonCrawler.EnemyAssenbly.Classes
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EnemyDomain
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
