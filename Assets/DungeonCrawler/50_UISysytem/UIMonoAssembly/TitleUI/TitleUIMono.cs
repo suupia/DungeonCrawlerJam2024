366072e50c55e7310c96412ace4735a403d795fa
suupia <morita777k@icloud.com>
2024-03-30 14:14:18 +0900

    Add temp TitleUI.prefab

---
+++ Assets/DungeonCrawler/50_UISysytem/UIMonoAssembly/TitleUI/TitleUIMono.cs
@@ -0,0 +1,13 @@
+#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUIMono : MonoBehaviour
+{
+    [SerializeField] CustomButton startButton = null!;
+    [SerializeField] CustomButton upgradeButton = null!;
+    [SerializeField] CustomButton settingsButton = null!;
+    
+    
+}
