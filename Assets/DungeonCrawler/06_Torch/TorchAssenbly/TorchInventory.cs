1cf1a0a02aaa656e5945fbe985be01c8aca16290
no-la <nzyc0216@icloud.com>
2024-03-30 16:50:56 +0900

    add value to TorchInventory

--- Assets/DungeonCrawler/06_Torch/TorchAssenbly/TorchInventory.cs
+++ Assets/DungeonCrawler/06_Torch/TorchAssenbly/TorchInventory.cs
@@ -3,6 +3,11 @@ namespace DungeonCrawler
 {
     public class TorchInventory
     {
-        
+        public int Value { get; set; }
+
+        public TorchInventory(int maxValue)
+        {
+            Value = 0;
+        }
     }
 }
