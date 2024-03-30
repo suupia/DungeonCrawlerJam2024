7a6c249aac7ec1b2d01415248426edbd321cc456
no-la <nzyc0216@icloud.com>
2024-03-30 14:14:16 +0900

    add UpdateTurn() content

--- Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerSystem.cs
+++ Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerSystem.cs
@@ -1,15 +1,28 @@
 #nullable enable
 using System;
+using VContainer;
 
 namespace DungeonCrawler
 {
     public class HangerSystem
     {
+        HangerMeter _hangerMeter;
+
+        int _turnDecrease = 1;
         
-        
+        public HangerSystem()
+        {
+            _hangerMeter = new HangerMeter(100);
+        }
+
         public void UpdateTurn(Action action) // from PlayerController.Move()?
         {
-            
+            _hangerMeter.Value -= _turnDecrease;
+
+            if (_hangerMeter.Value <= 0)
+            {
+                GameOver(action);
+            }
         }
 
         void GameOver(Action action)
