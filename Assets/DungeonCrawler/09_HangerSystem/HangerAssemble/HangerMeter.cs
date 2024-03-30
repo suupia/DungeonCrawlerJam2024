ba206d452f86e7f71bd90f76fb84060467effada
no-la <nzyc0216@icloud.com>
2024-03-30 13:59:37 +0900

    add params to HangerMeter

--- Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerMeter.cs
+++ Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerMeter.cs
@@ -4,5 +4,13 @@ namespace DungeonCrawler
 {
     public class HangerMeter
     {
+        int _maxValue;
+        public int Value { get; set; }
+
+        public HangerMeter(int maxValue)
+        {
+            _maxValue = maxValue;
+            Value = _maxValue;
+        }
     }
 }
