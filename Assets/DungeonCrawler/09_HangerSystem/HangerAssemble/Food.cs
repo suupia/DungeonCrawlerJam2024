33588eab20db80811cd6e3d05ffd5d8b70945984
no-la <nzyc0216@icloud.com>
2024-03-30 15:51:02 +0900

    add RemoveEntity() to OnEaten()

--- Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/Food.cs
+++ Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/Food.cs
@@ -3,6 +3,7 @@
 using System;
 using Codice.Client.GameUI.Explorer;
 using CodiceApp.EventTracking;
+using DungeonCrawler.MapAssembly.Classes;
 using DungeonCrawler.MapAssembly.Interfaces;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -12,12 +13,18 @@ namespace DungeonCrawler
     public class Food : IGridEntity
     {
         public event EventHandler OnEaten = (sender, e) => { };
+        public Func<(int x, int y)> GridPosition = () => (0, 0);
         public int HangerMeterIncreaseAmount { get; set; } = 11;
         HangerSystem _hangerSystem;
 
-        public Food(HangerSystem hangerSystem)
+        public Food(HangerSystem hangerSystem, DungeonSwitcher dungeonSwitcher)
         {
             _hangerSystem = hangerSystem;
+            OnEaten += (sender, e) =>
+            {
+                Debug.Log("Food.OnEaten()");
+                dungeonSwitcher.CurrentDungeon.Map.RemoveEntity(GridPosition().x,GridPosition().y, this);
+            };
         }
         public void GotOn()
         {
--- Assets/DungeonCrawler/09_HangerSystem/HangerMonoAssemble/FoodControllerMono.cs
+++ Assets/DungeonCrawler/09_HangerSystem/HangerMonoAssemble/FoodControllerMono.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using CodiceApp.EventTracking;
+using DungeonCrawler.MapAssembly.Classes;
 using UnityEngine;
 
 namespace DungeonCrawler
@@ -13,6 +14,7 @@ namespace DungeonCrawler
         public void Construct(Food food)
         {
             _food = food;
+            _food.GridPosition = () => GridConverter.WorldPositionToGridPosition(transform.position);
             _food.OnEaten += (sender, e) =>
             {
                 Destroy(gameObject);
