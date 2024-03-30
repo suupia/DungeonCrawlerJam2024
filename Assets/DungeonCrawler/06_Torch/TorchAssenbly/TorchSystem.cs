4f9bdaa040699c170710a801004c072ea9a4fe1f
no-la <nzyc0216@icloud.com>
2024-03-30 17:00:26 +0900

    add PlaceTorch(), PickUpTorch()

--- Assets/DungeonCrawler/06_Torch/TorchAssenbly/TorchSystem.cs
+++ Assets/DungeonCrawler/06_Torch/TorchAssenbly/TorchSystem.cs
@@ -1,8 +1,29 @@
 ﻿#nullable enable
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using VContainer;
+using Debug = UnityEngine.Debug;
+
 namespace DungeonCrawler
 {
     public class TorchSystem
     {
+        TorchInventory _torchInventory;
         
+        [Inject]
+        public TorchSystem(TorchInventory torchInventory)
+        {
+            _torchInventory = torchInventory;
+        }
+
+        public void PlaceTorch(int x, int y)
+        {
+            Debug.Log($"place a torch at ({x}, {y})");
+        }
+        public void PickUpTorch()
+        {
+            _torchInventory.Value += 1;
+        }
     }
 }
