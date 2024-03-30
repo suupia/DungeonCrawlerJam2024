d24a223b25be77f8288e2c2f64b6d806ac27a9e0
no-la <nzyc0216@icloud.com>
2024-03-30 17:16:42 +0900

    write TorchController.cs

--- Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchControllerMono.cs
+++ Assets/DungeonCrawler/06_Torch/TorchMonoAssenbly/TorchControllerMono.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DungeonCrawler.MapAssembly.Classes;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +8,15 @@ namespace DungeonCrawler
 {
     public class TorchControllerMono : MonoBehaviour
     {
+        Torch _torch;
+        
         public void Construct(Torch torch)
         {
-            
-        }
-    }
+            _torch = torch;
+            _torch.GridPosition = () => GridConverter.WorldPositionToGridPosition(transform.position);
+            _torch.OnPicked += (sender, e) =>
+            {
+                Destroy(gameObject);
+            };
+        }    }
 }
