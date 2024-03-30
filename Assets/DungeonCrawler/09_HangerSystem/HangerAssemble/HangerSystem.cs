bf3318dd00a0724bcf55c36a6f0b0d3e2799e286
no-la <nzyc0216@icloud.com>
2024-03-30 14:07:34 +0900

    add GameOver() to HangerSystem

--- Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerSystem.cs
+++ Assets/DungeonCrawler/09_HangerSystem/HangerAssemble/HangerSystem.cs
@@ -1,12 +1,20 @@
 #nullable enable
+using System;
 
 namespace DungeonCrawler
 {
     public class HangerSystem
     {
-        public void UpdateTurn() // from PlayerController.Move()
+        
+        
+        public void UpdateTurn(Action action) // from PlayerController.Move()?
         {
             
+        }
+
+        void GameOver(Action action)
+        {
+            action();
         }
     }
 }
