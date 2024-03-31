6e75205d5f99cd9b68c19394035b153c67a55c22
no-la <nzyc0216@icloud.com>
2024-03-31 10:58:48 +0900

    add setting upgradeText

--- Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeContentUIMono.cs
+++ Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeContentUIMono.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System;
+using DungeonCrawler._10_UpgradeSystem.UpgradeAssembly;
 using PlasticGui.WebApi.Responses;
 using TMPro;
 using UnityEngine;
@@ -18,17 +19,23 @@ namespace DungeonCrawler
         Func<int> CurrentValue = () => 0;
         Func<int> NextValue = () => 0;
 
+        FlamePoint _flamePoint;
+
         public void SetUp(string upgradeName, int cost, Action upgrade, Func<int> currentValue, Func<int> nextValue)
         {
-            _upgradeName = upgradeName!;
+            _upgradeName = upgradeName;
             _upgradeCost = cost;
             Upgrade = upgrade;
             CurrentValue = currentValue;
             NextValue = nextValue;
-            
+
+            upgradeText.text = _upgradeName;
             upgradeCustomButton.AddListener(() =>
             {
+                if (_flamePoint.FlamePointValue<cost) Debug.Log($"cancel upgrade {_upgradeName}, cost = {cost}, flamePoint = {_flamePoint.FlamePointValue}");
+                
                 Debug.Log($"upgrade {_upgradeName}");
+                Upgrade();
             });
         }
     }
