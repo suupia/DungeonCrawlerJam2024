8fa2f9e94b7494b3e001d908f42a8e90e4b89a08
suupia <morita777k@icloud.com>
2024-03-31 18:24:55 +0900

    Fix HangerGauge

--- Assets/DungeonCrawler/00_DungeonCrawlerSystem/GameSystemScope.cs
+++ Assets/DungeonCrawler/00_DungeonCrawlerSystem/GameSystemScope.cs
@@ -46,7 +46,7 @@ namespace DungeonCrawler
             // Enemy
             
             // Hanger
-            builder.Register<HangerMeter>(Lifetime.Scoped).WithParameter("maxValue",50);
+            builder.Register<HangerMeter>(Lifetime.Scoped).WithParameter("maxValue",500);
             builder.Register<HangerSystem>(Lifetime.Scoped);
 
             // FlamePoint
