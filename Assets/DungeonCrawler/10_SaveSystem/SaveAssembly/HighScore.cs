c3d0a3bd196db47e0f73009971b3ef2f5e4e70a6
no-la <nzyc0216@icloud.com>
2024-03-31 19:02:36 +0900

    add HighScore.cs and register to VContainer

---
+++ Assets/DungeonCrawler/10_SaveSystem/SaveAssembly/HighScore.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace DungeonCrawler._10_UpgradeSystem.UpgradeAssembly
+{
+    public class HighScore
+    {
+        const string SaveKey = "HighScore";
+        public int HighScoreValue
+        {
+            get =>  PlayerPrefs.GetInt(SaveKey, 0);
+            set =>  PlayerPrefs.SetInt(SaveKey, value);
+        }
+
+        public void GainFlamePoint(int value)
+        {
+            Debug.Log($"Gain HighScore {value}");
+            HighScoreValue += value;
+        }
+    }}
