f8efcf57b2d116bbe69d6687875722abb06639ae
no-la <nzyc0216@icloud.com>
2024-03-31 13:53:02 +0900

    save scene

--- Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeUIMono.cs
+++ Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeUIMono.cs
@@ -55,6 +55,7 @@ public class UpgradeUIMono : MonoBehaviour
                     break;
                 default:
                     Debug.LogWarning($"Unsupported UpgradeKind {upgradeKind}");
+                    break;
             }
         }
         
