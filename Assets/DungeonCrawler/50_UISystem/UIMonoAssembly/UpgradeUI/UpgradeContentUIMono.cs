bb74817bd01ccdd13fa48ab5365bbacf77eb379b
no-la <nzyc0216@icloud.com>
2024-03-31 10:45:01 +0900

    add properties to UpgradeContentUIMono

--- Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeContentUIMono.cs
+++ Assets/DungeonCrawler/50_UISystem/UIMonoAssembly/UpgradeUI/UpgradeContentUIMono.cs
@@ -1,10 +1,26 @@
 ﻿#nullable enable
+using System;
+using PlasticGui.WebApi.Responses;
 using UnityEngine;
 
 namespace DungeonCrawler
 {
     public class UpgradeContentUIMono : MonoBehaviour
     {
-        
+        string _upgradeName = null!;
+        int _upgradeCost = 0;
+
+        Action Upgrade = () => { };
+        Func<int> CurrentValue = () => 0;
+        Func<int> NextValue = () => 0;
+
+        public void SetUp(string upgradeName, int cost, Action upgrade, Func<int> currentValue, Func<int> nextValue)
+        {
+            _upgradeName = upgradeName!;
+            _upgradeCost = cost;
+            Upgrade = upgrade;
+            CurrentValue = currentValue;
+            NextValue = nextValue;
+        }
     }
 }
