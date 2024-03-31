4b5c7a629162a575f365497c7b31392dcfa6f35e
no-la <nzyc0216@icloud.com>
2024-03-31 10:01:49 +0900

    add FlamePoint.cs

---
+++ Assets/DungeonCrawler/10_UpgradeSystem/UpgradeAssembly/FlamePoint.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace DungeonCrawler._10_UpgradeSystem.UpgradeAssembly
+{
+    public class FlamePoint
+    {
+        const string SaveKey = "FlamePoint";
+        public int FlamePointValue
+        {
+            get =>  PlayerPrefs.GetInt(SaveKey, 0);
+            set =>  PlayerPrefs.SetInt(SaveKey, value);
+        }
+
+        public void GainFlamePoint(int value)
+        {
+            Debug.Log($"Gain FlamePoint {value}");
+            FlamePointValue += value;
+        }
+    }}
